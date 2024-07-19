2244d670a66af829709101050f57269f0a22a052
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-07-20 02:19:32 +0530

    fyers integrated successfully

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = (user, res) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+  const expiryDate = new Date(Date.now() + 3600000); 
 
   const { password, ...rest } = user._doc;
 
