38fb5e3b8965b15705af524335e3e882769e05da
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-07-25 19:28:46 +0530

    modified generate token

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,13 +1,20 @@
 import jwt from 'jsonwebtoken';
 
+export const cookieOption = {
+  maxAge: 1000 * 60 ,
+  sameSite: 'none',
+  httpOnly: true,
+  secure: true,
+};
+
 export const generateToken = (user, res) => {
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  const expiryDate = new Date(Date.now() + 3600000); 
+  // const expiryDate = new Date(Date.now() + 3600000); 
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {expiresIn:"30s" } );
 
   const { password, ...rest } = user._doc;
 
   res
-    .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+    .cookie("access_token", token,cookieOption)
     .status(200)
     .json(rest);
 };
