addc6a694dbec5c7acc540bbdd3ebbe59d2fa4ce
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-08-21 00:51:03 +0530

    updated activate bot API for intraday market and taking decision on positions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -55,8 +55,8 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
+//for cnc market
 // export const activateAutoTradeBot = async (req, res) => {
-//   // const { userId } = req.params;
 //   const { userId } = req.params;
 //   const { marginProfitPercentage, marginLossPercentage } = req.body;
 
@@ -86,7 +86,6 @@ export const deleteUser = async (req, res, next) => {
 //     await user.save();
 
 //     const autoTradeLoop = async () => {
-//       console.log("stoped", user.autoTradeBot);
 //       if (user.autoTradeBot === "stopped") {
 //         console.log("Auto-trade loop stopped");
 //         user.autoTradeBot = "inactive";
@@ -98,40 +97,28 @@ export const deleteUser = async (req, res, next) => {
 //       }
 
 //       const currentTime = getCurrentTime();
-//       console.log(currentTime);
-
-//       if (currentTime >= "09:30" && currentTime <= "19:30") {
-//         console.log("Within trading hours");
-//       } else {
-//         console.log("Outside trading hours");
+//       if (currentTime < "09:30" || currentTime > "23:30") {
+//         user.autoTradeBot = "inactive";
+//         await user.save();
+//         clearInterval(user.loopIntervalId);
+//         console.log(
+//           "Auto trading can only be activated between 9:30 AM and 4:30 PM"
+//         );
+//         return;
 //       }
 
 //       try {
-//         const currentTime = getCurrentTime();
-//         if (currentTime < "09:30" || currentTime > "19:30") {
-//           user.autoTradeBot = "inactive";
-//           await user.save();
-//           clearInterval(loopInterval);
-//           console.log(
-//             "Auto trading can only be activated between 9:30 AM and 4:30 PM"
-//           );
-//           return;
-//         }
-
 //         const [fundsResponse, holdingsResponse] = await Promise.all([
 //           axios.get(
-//             `http://localhost:8080/api/v1/fyers/fundsByUserId/${userId}`
+//             `https://api.stockgenius.ai/api/v1/fyers/fundsByUserId/${userId}`
 //           ),
 //           axios.get(
-//             `http://localhost:8080/api/v1/fyers/holdingsByUserId/${userId}`
+//             `https://api.stockgenius.ai/api/v1/fyers/holdingsByUserId/${userId}`
 //           ),
 //         ]);
 
 //         const funds = fundsResponse.data;
-//         // console.log(funds);
-
 //         const holdings = holdingsResponse.data;
-//         // console.log(holdings);
 
 //         const isFundsValid = checkFunds(funds);
 //         const areHoldingsValid = checkHoldings(holdings);
@@ -139,7 +126,7 @@ export const deleteUser = async (req, res, next) => {
 //         if (!isFundsValid || !areHoldingsValid) {
 //           user.autoTradeBot = "inactive";
 //           await user.save();
-//           clearInterval(loopInterval);
+//           clearInterval(user.loopIntervalId);
 //           console.log("Insufficient funds or no holdings");
 //           return;
 //         }
@@ -147,122 +134,121 @@ export const deleteUser = async (req, res, next) => {
 //         user.autoTradeBot = "running";
 //         await user.save();
 
-//         // Define the URL for the Python server
 //         const pythonServerUrl =
-//           "http://ec2-13-126-36-55.ap-south-1.compute.amazonaws.com:8000/autoTradingActivated";
-
-//         // Send a POST request to the Python server
+//           "http://ec2-13-232-40-122.ap-south-1.compute.amazonaws.com:8000/autoTradingActivated";
 //         const response = await axios.post(pythonServerUrl, {
 //           userId,
 //           marginProfit,
 //           marginLoss,
 //         });
 
-//         // Ensure the response is correct and contains the expected data structure
 //         if (response.data && Array.isArray(response.data)) {
-//           // Access the decision array
 //           const decisions = response.data[0].decision;
-
-//           // Loop through and log each decision object
-//           console.log("Decision Data:");
-//           decisions.forEach((decision, index) => {
-//             console.log(`Decision ${index + 1}:`, decision);
-//           });
-
-//           // Access the reinvestment array
 //           const reinvestmentData = response.data[1].reinvestment;
 
-//           // Loop through and log each reinvestment object
-//           console.log("Reinvestment Data:");
-//           reinvestmentData.forEach((reinvestment, index) => {
-//             console.log(`Reinvestment ${index + 1}:`, reinvestment);
-//           });
+//           console.log(decisions);
+//           console.log(reinvestmentData);
 
-//           // Combine decision and reinvestment data
 //           const combinedData = [
 //             ...decisions.map((decision) => ({
 //               symbol: decision.Symbol,
-//               qty: 1, // Default quantity for decision
-//               type: 2, // Order type (ensure this is correct for your API)
-//               side: decision.Decision === "Sell" ? -1 : 1, // Side: -1 for Sell, 1 for Buy
-//               productType: "CNC", // Product type (ensure this is correct for your API)
-//               limitPrice: 0, // Limit price (default or dynamic based on your requirements)
-//               stopPrice: 0, // Stop price (default or dynamic based on your requirements)
-//               disclosedQty: 0, // Disclosed quantity (default or dynamic based on your requirements)
-//               validity: "DAY", // Validity of the order
-//               offlineOrder: false, // Offline order (default or dynamic based on your requirements)
-//               stopLoss: 0, // Stop loss (default or dynamic based on your requirements)
-//               takeProfit: 0, // Take profit (default or dynamic based on your requirements)
+//               qty: decision.Quantity,
+//               type: 2,
+//               side: decision.Decision === "Sell" ? -1 : 1,
+//               productType: "CNC",
+//               limitPrice: 0,
+//               stopPrice: 0,
+//               disclosedQty: 0,
+//               validity: "DAY",
+//               offlineOrder: false,
+//               stopLoss: 0,
+//               takeProfit: 0,
 //               orderTag: "autotrade",
 //             })),
 //             ...reinvestmentData.map((reinvestment) => ({
 //               symbol: reinvestment.Symbol,
-//               qty: reinvestment.Quantity, // Quantity from reinvestment data
-//               type: 2, // Order type (ensure this is correct for your API)
-//               side: 1, // Always Buy for reinvestment
-//               productType: "CNC", // Product type (ensure this is correct for your API)
-//               limitPrice: 0, // Limit price (default or dynamic based on your requirements)
-//               stopPrice: 0, // Stop price (default or dynamic based on your requirements)
-//               disclosedQty: 0, // Disclosed quantity (default or dynamic based on your requirements)
-//               validity: "DAY", // Validity of the order
-//               offlineOrder: false, // Offline order (default or dynamic based on your requirements)
-//               stopLoss: 0, // Stop loss (default or dynamic based on your requirements)
-//               takeProfit: 0, // Take profit (default or dynamic based on your requirements)
+//               qty: reinvestment.Quantity,
+//               type: 2,
+//               side: 1,
+//               productType: "CNC",
+//               limitPrice: 0,
+//               stopPrice: 0,
+//               disclosedQty: 0,
+//               validity: "DAY",
+//               offlineOrder: false,
+//               stopLoss: 0,
+//               takeProfit: 0,
 //               orderTag: "autotrade",
 //             })),
 //           ];
 
-//           // Limit to 10 orders if needed
-//           // const orders = combinedData.slice(0, 10);
-//           const orders = combinedData;
+//           // Validate orders before sending
+//           const validatedOrders = combinedData
+//             .map((order, index) => {
+//               const { isValid, errors } = validateOrder(order);
+//               if (!isValid) {
+//                 console.log(
+//                   `Order validation failed at index ${index}:`,
+//                   errors
+//                 );
+//                 return null; // Exclude invalid orders
+//               }
+//               return order;
+//             })
+//             .filter((order) => order !== null); // Remove null entries
 
-//           console.log("Generated Orders:", orders);
-//           console.log("total orders : ", combinedData.length);
+//           console.log("Validated Orders:", validatedOrders);
+//           console.log("Total validated orders:", validatedOrders.length);
 
-//           orders.forEach((order, index) => {
-//             const { isValid, errors } = validateOrder(order);
-//             if (!isValid) {
-//               console.log(`Order validation failed at index ${index}:`, errors);
-//             }
-//           });
+//           const chunkSize = 10;
+//           const orderChunks = chunkArray(combinedData, chunkSize);
+
+//           for (const chunk of orderChunks) {
+//             console.log("Placing orders:", chunk);
+
+//             const accessToken = fyersUserDetails.accessToken;
+
+//             // try {
+//             //   const placeOrderResponse = await axios.post(
+//             //     `https://api.stockgenius.ai/api/v1/fyers/placeMultipleOrders/${userId}`,
+//             //     { accessToken, orders: chunk }
+//             //   );
+
+//             //   const { successfulOrders = [], unsuccessfulOrders = [] } =
+//             //     placeOrderResponse.data;
+
+//             //   if (successfulOrders.length > 0) {
+//             //     console.log("Successful orders:", successfulOrders);
+//             //   }
+
+//             //   if (unsuccessfulOrders.length > 0) {
+//             //     console.log("Failed orders:", unsuccessfulOrders);
+//             //   }
+//             // } catch (error) {
+//             //   console.error("Error placing orders:", error);
+//             //   throw error; // Re-throw to be caught by outer try-catch
+//             // }
+//           }
+
+//           user.autoTradeBot = "active";
+//           await user.save();
+
+//           console.log(
+//             "Orders placed successfully, auto trade bot set to active"
+//           );
 //         } else {
 //           console.error("Unexpected response format:", response.data);
 //         }
-
-//         const accessToken = fyersUserDetails.accessToken;
-
-//         const placeOrderResponse = await axios.post(
-//           `https://api.stockgenius.ai/api/v1/fyers/placeMultipleOrders/${userId}`,
-//           { accessToken, orders }
-//         );
-
-//         const { successfulOrders = [], unsuccessfulOrders = [] } =
-//           placeOrderResponse.data;
-
-//         if (successfulOrders.length > 0) {
-//           console.log("Successful orders:", successfulOrders);
-//         }
-
-//         if (unsuccessfulOrders.length > 0) {
-//           console.log("Failed orders:", unsuccessfulOrders);
-//         }
-
-//         user.autoTradeBot = "active";
-//         await user.save();
-
-//         console.log("Orders placed successfully, auto trade bot set to active");
-//         console.log("Success : ", successfulOrders);
-//         console.log("rejected : ", unsuccessfulOrders);
 //       } catch (error) {
 //         console.error("Error in auto trade loop:", error);
 //         user.autoTradeBot = "inactive";
 //         await user.save();
-//         clearInterval(loopInterval);
+//         clearInterval(user.loopIntervalId);
 //       }
 //     };
 
 //     // Start the loop with an interval of 10 sec
-//     const loopInterval = setInterval(autoTradeLoop, 10 * 1000);
+//     user.loopIntervalId = setInterval(autoTradeLoop, 10 * 1000);
 
 //     // Run the loop once immediately
 //     await autoTradeLoop();
@@ -276,8 +262,7 @@ export const deleteUser = async (req, res, next) => {
 //   }
 // };
 
-// Controller function to deactivate auto trade bot
-
+//for intraday market
 export const activateAutoTradeBot = async (req, res) => {
   const { userId } = req.params;
   const { marginProfitPercentage, marginLossPercentage } = req.body;
@@ -319,7 +304,7 @@ export const activateAutoTradeBot = async (req, res) => {
       }
 
       const currentTime = getCurrentTime();
-      if (currentTime < "09:30" || currentTime > "16:30") {
+      if (currentTime < "09:30" || currentTime > "23:30") {
         user.autoTradeBot = "inactive";
         await user.save();
         clearInterval(user.loopIntervalId);
@@ -330,22 +315,15 @@ export const activateAutoTradeBot = async (req, res) => {
       }
 
       try {
-        const [fundsResponse, holdingsResponse] = await Promise.all([
-          axios.get(
-            `https://api.stockgenius.ai/api/v1/fyers/fundsByUserId/${userId}`
-          ),
-          axios.get(
-            `https://api.stockgenius.ai/api/v1/fyers/holdingsByUserId/${userId}`
-          ),
-        ]);
+        const fundsResponse = await axios.get(
+          `https://api.stockgenius.ai/api/v1/fyers/fundsByUserId/${userId}`
+        );
 
         const funds = fundsResponse.data;
-        const holdings = holdingsResponse.data;
 
         const isFundsValid = checkFunds(funds);
-        const areHoldingsValid = checkHoldings(holdings);
 
-        if (!isFundsValid || !areHoldingsValid) {
+        if (!isFundsValid) {
           user.autoTradeBot = "inactive";
           await user.save();
           clearInterval(user.loopIntervalId);
@@ -357,7 +335,7 @@ export const activateAutoTradeBot = async (req, res) => {
         await user.save();
 
         const pythonServerUrl =
-          "http://ec2-13-126-36-55.ap-south-1.compute.amazonaws.com:8000/autoTradingActivated";
+          "http://ec2-13-232-40-122.ap-south-1.compute.amazonaws.com:8000/autoTradingActivated";
         const response = await axios.post(pythonServerUrl, {
           userId,
           marginProfit,
@@ -368,31 +346,33 @@ export const activateAutoTradeBot = async (req, res) => {
           const decisions = response.data[0].decision;
           const reinvestmentData = response.data[1].reinvestment;
 
-          console.log(decisions);
+          console.log("decisions : ", decisions);
           console.log(reinvestmentData);
 
           const combinedData = [
-            ...decisions.map((decision) => ({
-              symbol: decision.Symbol,
-              qty: decision.Quantity,
-              type: 2,
-              side: decision.Decision === "Sell" ? -1 : 1,
-              productType: "CNC",
-              limitPrice: 0,
-              stopPrice: 0,
-              disclosedQty: 0,
-              validity: "DAY",
-              offlineOrder: false,
-              stopLoss: 0,
-              takeProfit: 0,
-              orderTag: "autotrade",
-            })),
+            ...decisions
+              .filter((decision) => decision.Decision !== "Hold")
+              .map((decision) => ({
+                symbol: decision.Symbol,
+                qty: decision.Quantity,
+                type: 2,
+                side: decision.Decision === "Sell" ? -1 : 1,
+                productType: "INTRADAY",
+                limitPrice: 0,
+                stopPrice: 0,
+                disclosedQty: 0,
+                validity: "DAY",
+                offlineOrder: false,
+                stopLoss: 0,
+                takeProfit: 0,
+                orderTag: "autotrade",
+              })),
             ...reinvestmentData.map((reinvestment) => ({
               symbol: reinvestment.Symbol,
               qty: reinvestment.Quantity,
               type: 2,
               side: 1,
-              productType: "CNC",
+              productType: "INTRADAY",
               limitPrice: 0,
               stopPrice: 0,
               disclosedQty: 0,
