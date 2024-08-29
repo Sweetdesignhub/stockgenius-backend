44a6bede94da931837976a2bbcba9a7d79100852
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-08-29 12:29:35 +0530

    app.js commented api limiter

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 });
 
 // Routes declaration
-app.use('/api', apiLimiter);
+// app.use('/api', apiLimiter);
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/auth', authRoutes);
