d1c55ce252c1940b8997b7e69472a83b214beda1
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-09-03 16:04:45 +0530

    modified middleware

--- src/middlewares/validateAutoTradeBot.js
+++ src/middlewares/validateAutoTradeBot.js
@@ -7,11 +7,11 @@ export const validateAutoTradeBot = async (req, res, next) => {
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
-    if (!user.autoTradeBot) {
-      return res
-        .status(403)
-        .json({ error: 'User is not authorized for auto trading' });
-    }
+    // if (!user.autoTradeBot) {
+    //   return res
+    //     .status(403)
+    //     .json({ error: 'User is not authorized for auto trading' });
+    // }
     next();
   } catch (error) {
     res.status(500).json({ error: error.message });
